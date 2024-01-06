c2e89ece7d507a403e28dc512096564b656ea467
ypiard85 <60528207+ypiard85@users.noreply.github.com>
2024-01-06 17:41:16 +0100

    fix delete image

--- src/components/buttons/UplaodButton.tsx
+++ src/components/buttons/UplaodButton.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material"
+import { allowedExtensions } from "../../utils"
 
 interface PropsInterface{
     children: React.ReactNode,
@@ -10,7 +11,7 @@ const UpladButton: React.FC<PropsInterface> = ({children, onChange, multiple = t
 
     return (
         <Box  sx={{width: 'fit-content', position: 'relative'}} component="span">
-            <Box component="input" sx={{height: '100%', cursor: "pointer", opacity: 0, zIndex: 10, position: 'absolute', right: 0, left: 0}} type="file" multiple={multiple} onChange={onChange}></Box>
+            <Box component="input" sx={{height: '100%', cursor: "pointer", opacity: 0, zIndex: 10, position: 'absolute', right: 0, left: 0}} type="file" accept={allowedExtensions.join(',')} multiple={multiple} onChange={onChange}></Box>
             {children}
         </Box>
     )

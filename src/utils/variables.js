1f9a4c16c3727395326a0e2de148209304722edc
ypiard85 <60528207+ypiard85@users.noreply.github.com>
2024-04-16 17:12:04 +0200

    env change

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -3,8 +3,8 @@ import Cookies from 'js-cookie';
 export const BASE_URL = process.env.REACT_APP_BASE_URL
 export const BASE_URL_API = process.env.REACT_APP_API_BASE_URL
 
-export const IMAGE_PATH =  BASE_URL + '/storage/images/'
-export const AVATAR_PATH = BASE_URL + '/storage/images/avatars/'
+export const IMAGE_PATH =  BASE_URL + '/storage/public/images/'
+export const AVATAR_PATH = BASE_URL + '/storage/public/avatars/'
 
 export const UNKNOWN_USER = "Ancien membre"
 
